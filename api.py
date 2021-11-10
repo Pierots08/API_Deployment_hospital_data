1caf1a0d5cd68ce9895a5b3fd633ef6c91caa29a
Pierots08 <90680555+Pierots08@users.noreply.github.com>
2021-11-10 17:01:59 +0100

    fix first and second commit epic branch 1

--- api.py
+++ api.py
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 from src import covid_dash, hospitals_tb
-=======
+
 app = Flask(__name__)
 @app.route("/")
 def landing_page():
-return render_template('index.html')
+    return render_template('index.html')
 if __name__ == '__main__':
-app.run(host='0.0.0.0',port=1991, debug=True)
->>>>>>> branch-ticket-2
+    app.run(host='0.0.0.0',port=1991, debug=True)
