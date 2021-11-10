4c1e65dbbcb38b3db780144adbff232542124f13
Pierots08 <90680555+Pierots08@users.noreply.github.com>
2021-11-10 17:09:10 +0100

    fix git commit de epic

--- api.py
+++ api.py
@@ -8,9 +8,3 @@ def landing_page():
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1991, debug=True)
 
-@app.route("/dashboard")
-def dashboard():
-    return render_template('dashboard.html')
-@app.route("/map")
-def map():
-    return render_template('map.html')
