a1ccc45e1fbbcf1705673e035e81ada46b46945b
Pierots08 <90680555+Pierots08@users.noreply.github.com>
2021-11-10 16:56:17 +0100

    commit branch ticket 2

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
-=======
-from flask import Flask, render_template
-from src import covid_dash, hospitals_tb
->>>>>>> branch-ticket-1
+app = Flask(__name__)
+@app.route("/")
+def landing_page():
+return render_template('index.html')
+if __name__ == '__main__':
+app.run(host='0.0.0.0',port=1991, debug=True)
